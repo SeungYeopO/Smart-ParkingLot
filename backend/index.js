73774df9fe2fce0d8495f6688dd40bec8c61931f
황유경 <starinsky284@gmail.com>
2024-01-24 14:33:19 +0900

    feat: 웹소켓 통신 테스트 완료

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,42 @@
+// npm init  => 백엔드에 node.js 권한설정??
+// touch index.js => index.js 파일 생성 
+// ==== 백엔드 기본 서버는 돌릴 수 있는 상태 ===
+
+// npm install socket.io
+// npm install express
+
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
+//CORS 설정
+app.use(cors());
+
 io.on('connection', (socket) => {
     console.log('New client connected');
 
-    // 클라이언트로부터 메시지를 받았을 때의 처리
     socket.on('message', (message) => {
         console.log('Message received:', message);
-        // 메시지에 대한 응답 또는 처리
+       
     });
 
     socket.on('disconnect', () => {
         console.log('Client disconnected');
     });
+
+// 백엔드 서버의 변경사항이 있을때마다 emit 이벤트로 프론트에 메시지 보낼 수 있는 부분을 추가했음
+    socket.emit('backendMessage', '백엔드에서 온 메시지');
+
 });
 
-server.listen(3001, () => {
-    console.log('WebSocket server listening on port 3000');
+
+// 백엔드 포트 번호 입력
+server.listen(5000, () => {
+    console.log('WebSocket server listening on port 5000');
 });
