2a98adfbadf5fb11b6a77ae4d8cebd8820feacf3
황유경 <starinsky284@gmail.com>
2024-01-24 17:56:10 +0900

    feat: parkingStatus.jsx 컴포넌트 구현 중

--- frontend/front-end/src/App.jsx
+++ frontend/front-end/src/App.jsx
@@ -3,13 +3,16 @@ import './App.css'; // CSS 파일 임포트
 import ParkingLot from './components/ParkingLot';
 import MyCar from './components/MyCar';
 import WebSocket from './components/WebSocket';
+import DestinationStatus from './components/DestinationStatus';
+import ParkingStatus from './components/ParkingStatus';
 
 const App = () => {
   return (
     <div className='App'>
       {/* <ParkingLot /> */}
      {/* <MyCar /> */}
-     <WebSocket />
+     {/* <WebSocket /> */}
+     <ParkingStatus />
     </div>
   );
 };
