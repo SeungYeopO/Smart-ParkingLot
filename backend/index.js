5f6d61ffc8d3c12a46b7e48c8ace19d4c51762b7
양성호 <tipan33@naver.com>
2024-01-31 16:50:49 +0900

    fix : API 수정

--- backend/index.js
+++ backend/index.js
@@ -5,69 +5,25 @@ const app = express();
 
 const PORT = 3001;
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-  res.send("You need to request API")
-})
+app.get("/", (req, res) => {
+  res.send("You need to request API");
+});
 
-app.get('/api', (req, res)=>{
+app.get("/api", (req, res) => {
   res.send(`
     You need to request API detail
-    API list
-      /api/user : API for user
-      /api/manager : API for manager
-      /api/admin : API for administator
-  `)
-})
+  `);
+});
 
 //사용자와 직접 연관되는 API 명세
-app.get('/api/user/parking_sections', async (req, res)=>{
+app.get("/api/parking_sections/:lot_id", async (req, res) => {
+  const lot_id = req.params.lot_id;
   try {
-    const data = await pool.query("SELECT * FROM parking_sections");
-    return res.json(data[0]);
-  } catch (error) {
-    console.log(error);
-    return res.json(error);
-  }
-})
-
-//관리자가 사용하게 되는 API 명세
-app.get("/api/users", async (req, res) => {
-  try {
-    const data = await pool.query("SELECT * FROM users");
-    return res.json(data[0]);
-  } catch (error) {
-    console.log(error);
-    return res.json(error);
-  }
-});
-
-app.get("/api/user_cars", async (req, res) => {
-  try {
-    const data = await pool.query("SELECT * FROM user_cars");
-    return res.json(data[0]);
-  } catch (error) {
-    console.log(error);
-    return res.json(error);
-  }
-});
-
-app.get("/parking_lots", async (req, res) => {
-  try {
-    const data = await pool.query("SELECT * FROM parking_lots");
-    // console.log(data);
-    return res.json(data[0]);
-  } catch (error) {
-    console.log(error);
-    return res.json(error);
-  }
-});
-
-app.get("/parking_sections", async (req, res) => {
-  try {
-    const data = await pool.query("SELECT * FROM parking_sections");
-    // console.log(data);
+    const data = await pool.query(
+      `SELECT * FROM parking_sections WHERE lot_id = ${lot_id}`
+    );
     return res.json(data[0]);
   } catch (error) {
     console.log(error);
