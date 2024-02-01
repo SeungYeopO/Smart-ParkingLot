5ea86d70f1ff1105eaed02716dfcb5528a557fbe
양성호 <tipan33@naver.com>
2024-02-01 16:38:21 +0900

    fix : conflict 해결 작업

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,43 @@
 const express = require("express");
+const cors = require("cors");
+
+const pool = require("./DB.js");
+const app = express();
+
+const PORT = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("You need to request API");
+});
+
+app.get("/api", (req, res) => {
+  res.send(`
+    You need to request API detail
+  `);
+});
+
+//사용자와 직접 연관되는 API 명세
+app.get("/api/parking_sections/:lot_id", async (req, res) => {
+  const lot_id = req.params.lot_id;
+  try {
+    const data = await pool.query(
+      `SELECT * FROM parking_sections WHERE lot_id = ${lot_id}`
+    );
+    return res.json(data[0]);
+  } catch (error) {
+    console.log(error);
+    return res.json(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`서버 기동중`));
+
+
+/*
+const express = require("express");
 const {exec} = require("child_process");
 const pool = require("./DB.js");
 const mapLibrary = require("./mapLibrary.js");
@@ -105,3 +144,4 @@ app.get("/parking_sections", async (req, res) => {
 // });
 
 app.listen(PORT, () => console.log(`localhost:${PORT} 서버 기동중`));
+*/
