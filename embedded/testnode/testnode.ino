ea86f2716af4a5ecd6a76ba9f27fbf52200c20ce
유하림 <gkfla080@gmail.com>
2024-02-05 17:49:21 +0900

    RFfinal

--- embedded/testnode/testnode.ino
+++ embedded/testnode/testnode.ino
@@ -15,9 +15,10 @@ char pass[] = "1210@ssafy";
 int status = WL_IDLE_STATUS;
 
 char server[] = "3.38.208.90";
+String str = "1";
 
 unsigned long lastConnectionTime = 0;
-const unsigned long postingInterval = 1000L;
+const unsigned long postingInterval = 500L;
 
 WiFiEspClient client;
 
@@ -26,6 +27,7 @@ int cnt = 0;
 int DAT[7] = {0};
 
 void setup() {
+  //Serial.begin(9600);
   Serial1.begin(9600);
   WiFi.init(&Serial1);
 
@@ -35,51 +37,46 @@ void setup() {
   radio.startListening();
 
   if (WiFi.status() == WL_NO_SHIELD) {
+    //Serial.println("WiFi 쉴드 없음");
     while (true);
   }
 
   while (status != WL_CONNECTED) {
+    //Serial.print("WPA SSID에 연결 시도 중: ");
+    //Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
   }
+
+  //Serial.println("네트워크에 연결되었습니다.");
+
   printWifiStatus();
+
 }
 
 void loop() {
-  int num = 0;
-  char txt[32];
-  if (radio.available()) { // 만약 NRF 모듈에서 데이터가 들어오면
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    num = atoi(text);
-    DAT[num]++;
-    cnt++;
-  }
+  char txt[7] = { 0 };
+  str.toCharArray(txt, str.length()+1);
   // HTTP 요청
   unsigned long currentTime = millis();
-
-  if(cnt == 10){
-    int max = 0;
-    for(int i = 1; i < 7; ++i){
-      if(max < DAT[i]){
-        max = i;
-      }
-      DAT[i] = 0;
+  //Serial.print(txt);
+  if (currentTime - lastConnectionTime > postingInterval) {
+    if (!httpRequest(txt)) {
+     // Serial.println("HTTP 요청 실패. 재시도 중...");
+    } else {
+      lastConnectionTime = currentTime;
     }
-    txt[0] = max + '0';
-    if (currentTime - lastConnectionTime > postingInterval) {
-      if (!httpRequest(txt)) {
-      } else {
-        lastConnectionTime = currentTime;
-      }
-    }
-    cnt = 0;
   }
+  delay(100);
 }
 
 bool httpRequest(const char* text) {
+ // Serial.println();
+
   client.stop();
 
   if (client.connect(server, 3002)) {
+   // Serial.println("연결 중...");
+
     // "/updateDatabase" 엔드포인트에 GET 요청 전송
     client.print(F("GET /updateDatabase?text="));
     client.print(text);
@@ -92,15 +89,27 @@ bool httpRequest(const char* text) {
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
+        //Serial.write(c);
       }
     }
+
     return true;
   } else {
+   // Serial.println("연결 실패");
     return false;
   }
 }
 
 void printWifiStatus() {
+//  Serial.print("SSID: ");
+  ///Serial.println(WiFi.SSID());
+
   IPAddress ip = WiFi.localIP();
+ // Serial.print("IP 주소: ");
+  //Serial.println(ip);
+
   long rssi = WiFi.RSSI();
+ // Serial.print("신호 강도 (RSSI):");
+ // Serial.print(rssi);
+ // Serial.println(" dBm");
 }
--- embedded/uploaddb/uploaddb.ino
+++ embedded/uploaddb/uploaddb.ino
@@ -14,7 +14,7 @@ char ssid[] = "SSAFY_EMB";
 char pass[] = "1210@ssafy";
 int status = WL_IDLE_STATUS;
 
-char server[] = "192.168.100.131";
+char server[] = "3.38.208.90";
 
 unsigned long lastConnectionTime = 0;
 const unsigned long postingInterval = 500L;
@@ -26,7 +26,7 @@ String str = "";
 int cnt = 0;
 
 void setup() {
-  Serial.begin(9600);
+//  Serial.begin(9600);
   Serial1.begin(9600);
   WiFi.init(&Serial1);
 
@@ -36,17 +36,17 @@ void setup() {
   radio.startListening();
 
   if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi 쉴드 없음");
+//    Serial.println("WiFi 쉴드 없음");
     while (true);
   }
 
   while (status != WL_CONNECTED) {
-    Serial.print("WPA SSID에 연결 시도 중: ");
-    Serial.println(ssid);
+//    Serial.print("WPA SSID에 연결 시도 중: ");
+//    Serial.println(ssid);
     status = WiFi.begin(ssid, pass);
   }
 
-  Serial.println("네트워크에 연결되었습니다.");
+//  Serial.println("네트워크에 연결되었습니다.");
 
   printWifiStatus();
 
@@ -67,7 +67,7 @@ void loop() {
       str += text[0];
       cnt++;
     }
-    Serial.println(text);
+//    Serial.println(text);
   }
   // HTTP 요청
   unsigned long currentTime = millis();
@@ -75,10 +75,10 @@ void loop() {
   if(cnt == 4){
     char ch[7] = {0};
     str.toCharArray(ch, str.length()+1);
-    Serial.println(ch);
+ //   Serial.println(ch);
     if (currentTime - lastConnectionTime > postingInterval) {
       if (!httpRequest(ch)) {
-        Serial.println("HTTP 요청 실패. 재시도 중...");
+ //       Serial.println("HTTP 요청 실패. 재시도 중...");
       } else {
         lastConnectionTime = currentTime;
       }
@@ -90,18 +90,18 @@ void loop() {
 }
 
 bool httpRequest(const char* text) {
-  Serial.println();
+//  Serial.println();
 
   client.stop();
 
-  if (client.connect(server, 8080)) {
-    Serial.println("연결 중...");
+  if (client.connect(server, 3002)) {
+  //  Serial.println("연결 중...");
 
     // "/updateDatabase" 엔드포인트에 GET 요청 전송
     client.print(F("GET /updateDatabase?text="));
     client.print(text);
     client.println(F(" HTTP/1.1"));
-    client.println(F("Host: 192.168.100.131:8080"));
+    client.println(F("Host: 3.38.208.90:3002"));
     client.println("Connection: close");
     client.println();
 
@@ -109,27 +109,27 @@ bool httpRequest(const char* text) {
     while (client.connected()) {
       if (client.available()) {
         char c = client.read();
-        Serial.write(c);
+    //    Serial.write(c);
       }
     }
 
     return true;
   } else {
-    Serial.println("연결 실패");
+ //   Serial.println("연결 실패");
     return false;
   }
 }
 
 void printWifiStatus() {
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+//  Serial.print("SSID: ");
+//  Serial.println(WiFi.SSID());
 
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP 주소: ");
-  Serial.println(ip);
+//  Serial.print("IP 주소: ");
+//  Serial.println(ip);
 
   long rssi = WiFi.RSSI();
-  Serial.print("신호 강도 (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+//  Serial.print("신호 강도 (RSSI):");
+//  Serial.print(rssi);
+//  Serial.println(" dBm");
 }
