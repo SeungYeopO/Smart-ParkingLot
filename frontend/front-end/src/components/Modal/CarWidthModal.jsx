6cfc4f01d1d46b66a567c43cd7248651a116f099
Yugyeong <starinsky284@gmail.com>
2024-02-08 00:05:18 +0900

    feat:관리자 페이지 변수 데이터 따라 모달/알림 띄우는중

--- frontend/front-end/src/components/Modal/CarWidthModal.jsx
+++ frontend/front-end/src/components/Modal/CarWidthModal.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+// 우리 주차장의 칸 너비가 넓은 편이다 좁은 편이다를 나타내는 프리셋
+// 좁다 -> suv 같은 큰 차는 경차 옆으로 우선 배치해준다는걸 알려주는 모달
 
 const CarWidthModal = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
@@ -6,8 +8,7 @@ const CarWidthModal = ({ isOpen, onClose, children }) => {
   return (
     <div style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1000 }}>
       <div style={{ backgroundColor: 'darkgray', padding: '20px', borderRadius: '5px', width: '500px', maxHeight: '80vh', overflowY: 'auto' }}>
-        {children}
-        <button onClick={onClose}>닫기</button>
+        {children}   
       </div>
     </div>
   );
--- frontend/front-end/src/components/Modal/EndNavLogicModal.jsx
+++ frontend/front-end/src/components/Modal/EndNavLogicModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+// 경로 추천 완료되었을때 띄울 모달
 const EndNavLogicModal = () => {
     return (
         <div>
